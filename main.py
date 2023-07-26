ef7c774c0d7239891f3d2568f90c7c344702f590
springsandy <48716493+springsandy@users.noreply.github.com>
2023-07-26 17:21:07 +0900

    crime_data file add

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+mpl.rc('font', family='Malgun Gothic') #한글 폰트 지정
+plt.rcParams['axes.unicode_minus'] = False #마이너스 기호 깨짐 방지
 
+df1 = pd.read_csv('crime_data.csv', encoding='cp949')
+# df1.head()
+# df1.tail()
+# df1.info()
+# df1.index
+# df1.columns
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# df1['범죄대분류'].unique()
+# df1.groupby(['범죄대분류']).count()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#범죄대분류별로 그룹화
+df1_group = df1.groupby('범죄대분류').sum()
+print(df1_group)
